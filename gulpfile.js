78a02ff6d7991d53bd0e0e3e279d4c358552ab3f
David Lopez <davidlopez@streamlinemedia.com>
2020-01-30 16:52:40 -0800

    autoprefixer and sourcempas added, next auto-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps");
+
+// Put this after including our dependencies
+var paths = {
+    styles: {
+        // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
+        src: "*.scss",
+        // Compiled files will end up in whichever folder it's found in (partials are not compiled)
+        dest: "."
+    }
+
+    // Easily add additional paths
+    // ,html: {
+    //  src: '...',
+    //  dest: '...'
+    // }
+};
 
 
 // Define tasks after requiring dependencies
@@ -9,14 +29,19 @@ function style() {
     // (If you want to use scss files, simply look for *.scss files instead)
     return (
         gulp
-            .src("style.scss")
+            .src(paths.styles.src)
 
-            // Use sass with the files found, and log any errors
+            // Initialize sourcemaps before compilation starts
+            .pipe(sourcemaps.init())
             .pipe(sass())
             .on("error", sass.logError)
+            // Use postcss with autoprefixer and compress the compiled file using cssnano
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            // Now add/write the sourcemaps
+            .pipe(sourcemaps.write())
 
             // What is the destination for the compiled file?
-            .pipe(gulp.dest("."))
+            .pipe(gulp.dest(paths.styles.dest))
     );
 }
 
@@ -24,3 +49,13 @@ function style() {
 // This allows you to run it from the commandline using
 // $ gulp style
 exports.style = style;
+
+
+function watch(){
+    // gulp.watch takes in the location of the files to watch for changes
+    // and the name of the function we want to run on change
+    gulp.watch('style.scss', style)
+}
+
+// Don't forget to expose the task!
+exports.watch = watch
