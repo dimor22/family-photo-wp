942f689417f8b0e8b8c7a86c31b2f80d13766bb1
David Lopez <davidlopez@streamlinemedia.com>
2020-01-30 16:20:07 -0800

    first commit

---
+++ front-page.php
@@ -0,0 +1,360 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0.0
+ */
+
+get_header();
+?>
+
+    <main id="site-content" role="main">
+
+        <div class="screen">
+            <div class="foto-album">
+
+                <div class="unit-wrapper items-7">
+
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://petco.scene7.com/is/image/PETCO/1397583-right-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Amaira</li>
+                                <li>7</li>
+                                <li>12 Junio 2012</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/81tDOuJginL.png" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Benjamin</li>
+                                <li>4</li>
+                                <li>12 Junio 2015</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="unit-wrapper items-6">
+
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://petco.scene7.com/is/image/PETCO/1397583-right-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Amaira</li>
+                                <li>7</li>
+                                <li>12 Junio 2012</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/81tDOuJginL.png" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Benjamin</li>
+                                <li>4</li>
+                                <li>12 Junio 2015</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="unit-wrapper items-5">
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://petco.scene7.com/is/image/PETCO/1397583-right-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Amaira</li>
+                                <li>7</li>
+                                <li>12 Junio 2012</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/81tDOuJginL.png" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Benjamin</li>
+                                <li>4</li>
+                                <li>12 Junio 2015</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="unit-wrapper items-4">
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://petco.scene7.com/is/image/PETCO/1397583-right-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Amaira</li>
+                                <li>7</li>
+                                <li>12 Junio 2012</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/81tDOuJginL.png" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Benjamin</li>
+                                <li>4</li>
+                                <li>12 Junio 2015</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="unit-wrapper items-3">
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://petco.scene7.com/is/image/PETCO/1397583-right-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Amaira</li>
+                                <li>7</li>
+                                <li>12 Junio 2012</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/81tDOuJginL.png" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Benjamin</li>
+                                <li>4</li>
+                                <li>12 Junio 2015</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="unit-wrapper items-2">
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="unit">
+                        <img src="https://petco.scene7.com/is/image/PETCO/1397583-right-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Amaira</li>
+                                <li>7</li>
+                                <li>12 Junio 2012</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="unit-wrapper items-1">
+                    <div class="unit">
+                        <img src="https://s7d1.scene7.com/is/image/PETCO/1031740-left-1" alt="">
+                        <div class="details">
+                            <ul>
+                                <li>Shaila</li>
+                                <li>8</li>
+                                <li>13 Junio 2011</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+
+
+
+    </main><!-- #site-content -->
+
+
+<?php
+get_footer();
---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+
+/**
+ * CPT FAMILY MEMBERS
+ */
+function familiares_init() {
+    $args = array(
+        'labels'      => array(
+            'name'          => __( 'Familiares', 'labor-app' ),
+            'singular_name' => __( 'Familiar', 'labor-app' ),
+            'add_new_item'  => __( 'Nuevo Familiar', 'labor-app' ),
+            'edit_item'     => __( 'Edita Familiar', 'labor-app' )
+        ),
+        'public'      => true,
+        'menu_icon'   => 'dashicons-admin-users',
+        'has_archive' => true,
+        'rewrite'     => true,
+        'query_var'   => true,
+        'supports'    => array( 'title', 'editor', 'custom-fields', 'post-formats' )
+    );
+    register_post_type( 'Familiares', $args );
+}
+add_action( 'init', 'familiares_init' );
