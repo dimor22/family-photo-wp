81fa255cda38ab2260a725b8c60a141925a13086
David Lopez <davidlopez@streamlinemedia.com>
2020-02-05 12:04:12 -0800

    extracted logic out to its own plugin, changed some css and added some translations

--- templates/create-familiar-form.php
+++ templates/create-familiar-form.php
@@ -2,38 +2,10 @@
 
 /* Template Name: Familiar Registration */
 
-if( ! is_user_logged_in() ) {
+if( false === is_user_logged_in() ) {
     wp_redirect( wp_login_url() );
     exit;
 }
-
-function my_pre_save_post( $post_id ) {
-
-
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
-    die();
-
-
-    // Create a new post
-    $post = array(
-        'post_title'  => 'Mario' ,
-    );
-
-    // insert the post
-    $post_id = wp_insert_post( $post );
-
-    // return the new ID
-    return $post_id;
-
-}
-
-// add_filter('acf/pre_save_post' , 'my_pre_save_post', 10, 1 );
-
-
-
-
 
 acf_form_head();
 
@@ -56,16 +28,16 @@ get_header();
 
                     acf_form([
                             'id'    => 'add-new-familiar',
-                        'post_id'   => 'new_post',
-                        'new_post' => [
-                            'post_type' => 'familiares',
-                            'post_status'   => 'publish'
-                        ],
-                        'submit_value' => __('Add Post', 'family-pictures'),
-                        'honeypot' => true,
-                        'html_submit_spinner' => '<span class="acf-spinner"></span>',
-                        'updated_message' => __("Familiar Added", 'acf'),
-                        'uploader' => 'basic'
+                            'post_id'   => 'new_post',
+                            'new_post' => [
+                                'post_type' => 'familiares',
+                                'post_status'   => 'publish'
+                            ],
+                            'submit_value' => __('Add Family Member', 'lws-family-photo-library'),
+                            'honeypot' => true,
+                            'html_submit_spinner' => '<span class="acf-spinner"></span>',
+                            'updated_message' => __("Family Member Added", 'lws-family-photo-library'),
+                            'uploader' => 'basic'
                     ]);
 
                 endwhile;
